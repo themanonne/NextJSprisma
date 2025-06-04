9710a4977bd4549240f9215316e46011a446fc55
Manon <manon.mayr@gmail.com>
2025-06-04 13:46:01 +0200

    fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-8">Homepage</h1>
       <Link href="/posts" className="text-blue-600">Voir tous les posts <br /></Link>
       <Link href="/posts/new" className="text-blue-600">Créer un Post</Link>
-      <Link href="/users" className="text-blue-600">Vois tous les utilisateurs</Link>
+      <Link href="/users" className="text-blue-600">Voir tous les utilisateurs</Link>
       <Link href="/users/new" className="text-blue-600">Créer un Utilisateur</Link>
     </div >
   );
